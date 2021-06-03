316dbc0584b855204210113a475dbc5f8c8171e6
MrG. Asatryan <51170752+grishasatrian@users.noreply.github.com>
2021-06-03 17:29:28 +0400

    Project created, adding all files in program

---
+++ Contact/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Contact
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public Contact(string name, string email, string phone, string address)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.Phone = phone;
+            this.Address = address;
+        }
+    }
+}
